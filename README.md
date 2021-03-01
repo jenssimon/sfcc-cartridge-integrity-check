68a95cf6237bed5fd2cb859091f7914c24ef3dd7
Jens Simon <jens.simon80@googlemail.com>
2021-03-01 15:07:33 +0100

    Initial commit for version 2.0.0

---
+++ README.md
@@ -0,0 +1,65 @@
+[![NPM version][npm-image]][npm-url] [![Downloads][npm-downloads-image]][npm-url] [![Dependencies][deps-image]][deps-url] [![star this repo][gh-stars-image]][gh-url] [![fork this repo][gh-forks-image]][gh-url] [![Build Status][travis-image]][travis-url] ![Code Style][codestyle-image]
+
+# sfcc-cartridge-integrity-check
+
+> Check the integrity of SFCC cartridges that shouldn't be modified.
+
+![Screenshot](https://github.com/jenssimon/sfcc-cartridge-integrity-check/raw/master/screenshot.png)
+
+## Install
+
+```sh
+$ yarn add sfcc-cartridge-integrity-check --dev
+```
+
+## Usage
+
+Salesforce Commerce Cloud (SFCC) introduced the concept of cartridges that shouldn't be modified with their Storefront Reference Architecture (SFRA).
+
+To protect your customization projects from (unintented) modifications of such cartridges here is a small tool to check these cartridges.
+The git hash of the last commit in the cartridge is used to check if there is a modification. Also the uncommitted files will be checked and a warning will be shown.
+
+```javascript
+const cartridgeIntegrity = require('./cartridgeIntegrity');
+
+// retrieve this data from a file
+const integrityData = {
+  'app_storefront_base': 'SHA',
+};
+
+const { checkCartridgeIntegrity, generateCartridgeIntegrityDataFile } = cartridgeIntegrity({
+  readOnlyCartridges: [
+    'app_storefront_base',
+  ],
+  integrityData,
+  customizationProject: true, // if false integrity won't be checked
+});
+
+checkCartridgeIntegrity();
+```
+
+You can generate a file containing the integrity data as JSON.
+
+```javascript
+generateCartridgeIntegrityDataFile('./path/to/file.json');
+```
+
+## License
+
+MIT © 2021 [Jens Simon](https://github.com/jenssimon)
+
+[npm-url]: https://www.npmjs.com/package/sfcc-cartridge-integrity-check
+[npm-image]: https://badgen.net/npm/v/sfcc-cartridge-integrity-check
+[npm-downloads-image]: https://badgen.net/npm/dw/sfcc-cartridge-integrity-check
+
+[deps-url]: https://david-dm.org/jenssimon/sfcc-cartridge-integrity-check
+[deps-image]: https://badgen.net/david/dep/jenssimon/sfcc-cartridge-integrity-check
+
+[gh-url]: https://github.com/jenssimon/sfcc-cartridge-integrity-check
+[gh-stars-image]: https://badgen.net/github/stars/jenssimon/sfcc-cartridge-integrity-check
+[gh-forks-image]: https://badgen.net/github/forks/jenssimon/sfcc-cartridge-integrity-check
+
+[travis-url]: https://travis-ci.com/jenssimon/sfcc-cartridge-integrity-check
+[travis-image]: https://travis-ci.com/jenssimon/sfcc-cartridge-integrity-check.svg?branch=master
+
+[codestyle-image]: https://badgen.net/badge/code%20style/airbnb/f2a
---
+++ index.ts
@@ -0,0 +1,111 @@
+import fs from 'fs';
+import shell from 'shelljs';
+import chalk from 'chalk';
+import { table, getBorderCharacters } from 'table';
+
+type IntegrityData = {
+  [index: string]: string;
+}
+
+type CartrideIntegrityCheckParams = {
+  readOnlyCartridges: string[];
+  integrityData: IntegrityData;
+  customizationProject: boolean;
+}
+
+type CartridgeIntegrityCheckReturn = {
+  checkCartridgeIntegrity: () => void;
+  generateCartridgeIntegrityDataFile: (filename: string) => void;
+}
+
+const getDirHashes = (readOnlyCartridges: string[]) => readOnlyCartridges.reduce<{ [index: string]: string }>(
+  (acc, cartridge) => {
+    const res = shell.exec(`git rev-list -1 HEAD -- cartridges/${cartridge}/`, { silent: true });
+    acc[cartridge] = res.stdout.trim();
+    return acc;
+  }, {},
+);
+
+const getUncommitted = (cartridge: string) => [
+  shell.exec(`git diff --name-only cartridges/${cartridge}/`, { silent: true }),
+  shell.exec(`git diff --name-only --cached cartridges/${cartridge}/`, { silent: true }),
+]
+  .map(({ stdout }) => stdout.trim().split('\n'))
+  .reduce((acc, val) => acc.concat(val), [])
+  .filter((line) => !!line.trim())
+  .length;
+
+const listNotAllowedCommitsForCartridge = (cartridge: string, hash: string) => shell.exec(
+  `git log --color --format="%C(auto)%H %Cgreen%aN <%aE> %C(auto)%s" ${hash}..HEAD -- cartridges/${cartridge}/`,
+  { silent: true },
+).stdout;
+
+const checkCartridgeIntegrity = (
+  readOnlyCartridges: string[],
+  currIntegrityData: IntegrityData,
+  customizationProject: boolean,
+): void => {
+  if (customizationProject) {
+    const hashes = getDirHashes(readOnlyCartridges);
+    const modifiedCartridges = [];
+    const uncommittedChanges = [];
+    const integrityData: { [index: string]: { hash: string, uncommitted: number }} = {};
+    Object.entries(hashes).forEach(([cartridge, hash]) => {
+      integrityData[cartridge] = {
+        hash,
+        uncommitted: getUncommitted(cartridge),
+      };
+    });
+    Object.entries(integrityData).forEach(([cartridge, { hash, uncommitted }]) => {
+      const ok = hash === currIntegrityData[cartridge];
+      const isUncommitted = !!uncommitted;
+      process.stdout.write(`Integrity for ${cartridge} - ${
+        ok ? chalk.bold.green('OK ✅') : chalk.bold.red('FAIL ❌')
+      }${uncommitted ? ` - ${chalk.bold.yellow(`${uncommitted} uncommitted file(s)`)}` : ''}\n`);
+      if (!ok) {
+        modifiedCartridges.push(cartridge);
+        process.stdout.write(`\n${listNotAllowedCommitsForCartridge(cartridge, currIntegrityData[cartridge])}\n`);
+      }
+      if (isUncommitted) {
+        uncommittedChanges.push(cartridge);
+      }
+    });
+    if (modifiedCartridges.length) {
+      process.stdout.write(chalk.bold.red('\n🛑 Some read only cartridges are modified!!!\n'));
+    }
+    if (uncommittedChanges.length) {
+      process.stdout.write(chalk.bold.yellow('✋ You have uncommitted changes in read-only cartridge(s)!\n'));
+      process.stdout.write(chalk.yellow('\nDo you really want to modify these cartridge(s)?\n'));
+    }
+  } else {
+    // eslint-disable-next-line max-len
+    process.stdout.write(chalk.bold.yellow('🙈 Ignoring cartridge integrity check since this is not a customization project.\n'));
+  }
+};
+
+const generateCartridgeIntegrityDataFile = function (readOnlyCartridges: string[], filename: string) {
+  const hashes = getDirHashes(readOnlyCartridges);
+  process.stdout.write('Generated hashes for:\n');
+  const data: [string, string][] = [];
+  Object.entries(hashes).forEach(([cartridge, hash]) => {
+    data.push([chalk.bold.whiteBright(cartridge), hash]);
+  });
+  process.stdout.write(table(data, {
+    border: getBorderCharacters('void'),
+    columnDefault: {
+      paddingLeft: 0,
+      paddingRight: 1,
+    },
+    drawHorizontalLine: () => false,
+  }));
+  fs.writeFileSync(filename, `${JSON.stringify(hashes, undefined, 2)}\n`);
+};
+
+export = ({
+  readOnlyCartridges,
+  integrityData,
+  customizationProject,
+}: CartrideIntegrityCheckParams): CartridgeIntegrityCheckReturn => ({
+  checkCartridgeIntegrity: checkCartridgeIntegrity.bind(null, readOnlyCartridges, integrityData, customizationProject),
+  generateCartridgeIntegrityDataFile: generateCartridgeIntegrityDataFile.bind(null, readOnlyCartridges),
+});
